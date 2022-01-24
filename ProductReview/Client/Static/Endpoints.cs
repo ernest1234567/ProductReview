a6e7b50a1fbe0819ae916b2205c03684f9ca76df
Jeremy <2001784D@student.tp.edu.sg>
2022-01-24 15:03:10 +0800

    l

---
+++ ProductReview/Client/Static/Endpoints.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace ProductReview.Client.Static
+{
+    public static class Endpoints
+    {
+        private static readonly string Prefix = "api";
+
+        public static readonly string RestaurantsEndpoint = $"{Prefix}/Restaurants";
+    }
+}
